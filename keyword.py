81cf8e80ee3ac2b16dd1d279c656b3ab7f588f7b
katiezzzzz <47574900+katiezzzzz@users.noreply.github.com>
2019-07-22 13:33:28 +0100

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+# Create a BeautifulSoup object
+def getText(arg):
+    page = requests.get(arg)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    text = soup.get_text()
+    withoutspace = text.replace('\n\n',' ')
+    return withoutspace.strip()
+
+
+def keyword(arg,keyword):
+    if keyword in arg:
+        arg = arg.split('\n')
+        for line in arg:
+            if keyword in line:
+                print(line)
+    else:
+        pass
+    return
+
+keyword(getText('https://www.cnmv.es/portal/HR/HRAldia.aspx?lang=en'),'Warrants')
