913076517a16d04656899e6747842135559f959b
katiezzzzz <47574900+katiezzzzz@users.noreply.github.com>
2019-07-23 14:02:43 +0100

    Update web_scrapping.py

--- web_scrapping.py
+++ web_scrapping.py
@@ -126,11 +126,28 @@ example.initializeCache()
 example.generateCache()
 
 # use self.compare to update every 5 min
+
+class RepeatEvery(threading.Thread):
+    def __init__(self, interval, func, *args, **kwargs):
+        threading.Thread.__init__(self)
+        self.interval = interval  # seconds between calls
+        self.func = func          # function to call
+        self.args = args          # optional positional argument(s) for call
+        self.kwargs = kwargs      # optional keyword argument(s) for call
+        self.runable = True
+    def run(self):
+        while self.runable:
+            self.func(*self.args, **self.kwargs)
+            time.sleep(self.interval)
+    def stop(self):
+        self.runable = False
+
 def update(arg):
-    threading.Timer(300.0, update).start()
     arg.compare()
     arg.alert()
     arg.generateCache()
 
-# need to generate alert
-update(example)
+thread = RepeatEvery(10, update, example)
+print("starting")
+thread.start()
+
